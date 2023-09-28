66182705abf0c8c8f1d5b96cdfcf60c15c97b292
suzonahamed <144415810+suzonahamed@users.noreply.github.com>
2023-09-28 23:28:20 +0600

    Add files via upload

---
+++ slideShow/script.js
@@ -0,0 +1,22 @@
+
+const elements=document.querySelectorAll(".slideshow-element");
+
+let  count=1;
+
+setInterval(()=>{
+    count++;
+      let currentElement=document.querySelector(".current");
+
+      currentElement.classList.remove("current");
+
+      if(count>elements.length){
+        elements[0].classList.add("current");
+        count=1;
+      }
+      else{
+        currentElement.nextElementSibling.classList.add("current");
+      }
+
+      
+
+},1000);
